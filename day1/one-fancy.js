0c77ca49711c2c9e891001bc40d529f6a19a5726
LebsterFace <samsalinsky@gmail.com>
2021-12-01 05:29:47 +0000

    Rename + Add fancy versions

---
+++ day1/one-fancy.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+process.chdir(__dirname);
+
+const {increaseCount} = fs
+	.readFileSync("input.txt", "utf-8")
+	.split(/\n/g)
+	.map(str => parseInt(str))
+	.reduce(({last, increaseCount}, current) => ({
+		last: current,
+		increaseCount: increaseCount + (current > last ? 1 : 0)
+	}), {
+		last: Infinity, // The first one should not be counted
+		increaseCount: 0
+	});
+
+console.log(increaseCount);
