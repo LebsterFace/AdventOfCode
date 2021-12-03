f72c02f215caf343b55b195e7a8eff8fd15eb5e6
LebsterFace <samsalinsky@gmail.com>
2021-12-03 05:43:08 +0000

    Day 3: Binary Diagnostic

---
+++ solutions/day3/one.js
@@ -0,0 +1,26 @@
+process.chdir(__dirname);
+
+const input = require("fs")
+	.readFileSync("./input.txt", "utf-8")
+	.split(/\r?\n/g);
+
+// Positive = More 1s than 0s
+// Negative = More 0s than 1s
+// Zero = Same number of 1s and 0s
+const counts = Array.from({length: 12}, () => 0);
+for (const number of input) {
+	for (let i = 0; i < number.length; i++) {
+		if (number[i] === "1") {
+			counts[i]++;
+		} else {
+			counts[i]--;
+		}
+	}
+}
+
+// Positive = 1
+// Negative = 0
+const gamma = parseInt(counts.map(v => v > 0 ? 1 : 0).join(""), 2);
+const epsilon = parseInt(counts.map(v => v < 0 ? 1 : 0).join(""), 2);
+
+console.log(gamma * epsilon);
