7cd1d4ee2f4b6969948e609716398d45a966413e
LebsterFace <samsalinsky@gmail.com>
2021-12-04 08:12:51 +0000

    Optimise

--- solutions/day4/both-optimised.js
+++ solutions/day4/both-optimised.js
@@ -25,25 +25,30 @@ for (const number of numbers) {
 	boards: for (let boardNum = boards.length - 1; boardNum >= 0; boardNum--) {
 		const board = boards[boardNum];
 		for (const row of board) {
+			let marked = null;
 			for (let i = 0; i < row.length; i++) {
-				if (row[i] !== number) continue; // If the current cell is not the number, move to the next cell
-				row[i] = -1; // If it is, mark it
-
-				const hasWon = row.every(column => column < 0) || board.every(row => row[i] < 0);
-				if (!hasWon) continue; // If the board has not won, move to the next cell
-				
-				// Calculate the score of the board
-				const score = board
-					.flat()
-					.filter(n => n > 0)
-					.reduce((sum, current) => sum + current, 0) * number;
-
-				if (first === null) first = score; // If the first has not been found, this is the first
-				last = score; // This is also the last
-
-				boards.splice(boardNum, 1); // Remove the current board
-				continue boards; // Move to the next board
+				if (row[i] === number) {
+					row[i] = -1;
+					marked = i;
+				}
 			}
+
+			if (marked === null || !(
+				row.every(column => column < 0) ||
+				board.every(row => row[marked] < 0)
+			)) continue; // If the board has not won, move to the next cell
+
+			// Calculate the score of the board
+			const score = board
+				.flat()
+				.filter(n => n > 0)
+				.reduce((sum, current) => sum + current, 0) * number;
+
+			if (first === null) first = score; // If the first has not been found, this is the first
+			last = score; // This is also the last
+
+			boards.splice(boardNum, 1); // Remove the current board
+			continue boards; // Move to the next board
 		}
 	}
 }
