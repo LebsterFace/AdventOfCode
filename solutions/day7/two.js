79bbf9c0f79de6f31b1f93772e25257a83d47e8c
LebsterFace <samsalinsky@gmail.com>
2021-12-07 05:07:26 +0000

    Day 7: The Treachery of Whales

---
+++ solutions/day7/two.js
@@ -0,0 +1,29 @@
+process.chdir(__dirname);
+const input = require("fs")
+	.readFileSync("./input.txt", "utf-8")
+	.trim()
+	.split(",")
+	.map(Number);
+
+const alignTo = n => {
+	let result = 0;
+	for (const horizontalPosition of input) {
+		const k = Math.abs(horizontalPosition - n);
+		result += k * (k + 1) / 2;
+	}
+
+	return result;
+};
+
+const min = Math.min(...input);
+const max = Math.max(...input);
+let lowest = Infinity;
+
+for (let i = min; i <= max; i++) {
+	const result = alignTo(i);
+	if (result < lowest) {
+		lowest = result;
+	}
+}
+
+console.log(lowest);
