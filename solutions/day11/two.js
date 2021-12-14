a0141c4c9fff03b80917bfb3e39f20102832d9be
LebsterFace <samsalinsky@gmail.com>
2021-12-14 22:46:42 +0000

    Day 11: Add comments for part two

--- solutions/day11/two.js
+++ solutions/day11/two.js
@@ -22,6 +22,7 @@ const allFlashed = () => {
 };
 
 const step = () => {
+	// Reset every cell
 	for (let y = 0; y < input.length; y++) {
 		for (let x = 0; x < input[y].length; x++) {
 			input[y][x].value += 1;
@@ -29,16 +30,20 @@ const step = () => {
 		}
 	}
 
+	// Initialise the queue to all cells which flash immediately
 	const queue = input.flat().filter(cell => cell.value > 9);
-	if (queue.length === 0) return false;
+	if (queue.length === 0) return false; // Fastpath
 
+	// Until the queue is empty
 	while (queue.length > 0) {
-		const cell = queue.shift();
-		if (cell.flashed) continue;
+		const cell = queue.shift(); // Take the first item out of the queue
+		if (cell.flashed) continue; // If the cell has already flashed, it cannot do so again
 
 		cell.flashed = true;
+		// Reset the value
 		cell.value = 0;
 
+		// Get all unflashed neighbours
 		const neighbours = [
 			input[cell.y - 1]?.[cell.x - 1],
 			input[cell.y - 1]?.[cell.x],
@@ -50,9 +55,11 @@ const step = () => {
 			input[cell.y + 1]?.[cell.x + 1],
 		].filter(t => typeof t !== "undefined").filter(n => !n.flashed);
 
+		// Increment every neighbour
 		for (const n of neighbours)
 			n.value += 1;
 
+		// Add all neighbours who are now flashing to the queue
 		for (const n of neighbours) {
 			if (n.value > 9) {
 				queue.push(n);
